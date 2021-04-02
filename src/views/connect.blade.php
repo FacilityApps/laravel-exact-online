83480ade4adf13b1cb1be1226bcdcd8abe1e7215
Calvin Baart <51708327+calvinfa@users.noreply.github.com>
2021-04-02 12:46:56 +0200

    Update connect.blade.php

--- src/views/connect.blade.php
+++ src/views/connect.blade.php
@@ -1,4 +1,4 @@
-<form method="post" action="{{ route('exact.authorize') }}">
+<form method="post" action="{{ route(config('laravel-exact-online.exact_authorize', 'exact.authorize')) }}">
     {{ csrf_field() }}
     <button class="button small orange" type="submit">Verbinden met Nationale Interim Bank Exact App</button>
 </form>
